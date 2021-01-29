d95197e81b4e77f10408cf04147a1e95f1899a7e
David Muraco <dam6549@g.harvard.edu>
2021-01-29 02:50:50 -0700

    looks complete!

--- fibonacci.py
+++ fibonacci.py
@@ -15,13 +15,17 @@ def last_8(some_int):
 
 
 
-def optimized_fibonacci(f):
+def optimized_fibonacci(fib):
+    """Calculates a fibonacci value for a given number
 
+    :param int some_int: the number
+    :rtype: int
+    """
     accum = 0
     past1 = 1
     past2 = 1
 
-    for z in range(f):
+    for value in range(fib):
         past2 = past1
         past1 = accum
         accum = past2 + past1
@@ -36,30 +40,59 @@ class SummableSequence(object):
 
 
     def __call__(self, i):
-        # print("inside call")
-        # print(self.my_lst)
+        """Calculates a fibonacci value for a given number
 
-        for z in range(i-1):
-            # print(self.my_lst)
-            # print((sum(self.my_lst)))
+        :param int i: the sequence length
+        :rtype: int
+        """
+
+        start = len(self.my_lst) - 2
+
+        for z in range(start, i-1):
             self.my_lst.append(sum(self.my_lst))
             self.my_lst.pop(0)
-
-        return (self.my_lst[-1])
+        return self.my_lst[-1]
 
 
 if __name__ == "__main__":
 
-    print("f(100000)[-8:]", last_8(optimized_fibonacci(100000)))
+    print("f optimized fib ", last_8(optimized_fibonacci(100000)))
+    #
+    #
+    # new_seq = SummableSequence(0, 1)
+    # print("fib seq[-8:]:", last_8(new_seq(8)))
 
-    new_seq = SummableSequence(0, 1)
-    print("new_seq(100000)[-8:]:", last_8(new_seq(100000)))  # 60500327
+    # f(100,000) = 28746875
+    # im passing 78790626
+    #
+    # new_seq = SummableSequence(0, 1)
+    # print("fib seq 999999[-8:]:", last_8(new_seq(99999)))
+    #
+    # new_seq = SummableSequence(0, 1)
+    # print("fib seq 100000[-8:]:", last_8(new_seq(100000)))
+    #
+    # new_seq = SummableSequence(0, 1)
+    # print("fib seq 100001[-8:]:", last_8(new_seq(100001)))
 
+
+    # new_seq = SummableSequence(1, 2, 3)
+    # print("new_seq(5):", last_8(new_seq(5)))
+    #
     # new_seq = SummableSequence(5, 7, 11)
-    # print("new_seq(100000)[-8:]:", last_8(new_seq(100000)))  # 60500327
-
+    # print("new_seq(5) 5,7,11:", last_8(new_seq(5)))
+    #
+    #
     # new_seq = SummableSequence(5, 7, 11)
-    # print("new_seq(100000)[-8:]:", last_8(new_seq(99999)))  # 64224133
-
+    # print("new_seq(99998)[-8:]:", last_8(new_seq(99998)))  # 64224133
+    #
     # new_seq = SummableSequence(5, 7, 11)
-    # print("new_seq(100000)[-8:]:", last_8(new_seq(100001)))  # 87834603
+    # print("new_seq(99999)[-8:]:", last_8(new_seq(99999)))  # 64224133
+    #
+    new_seq = SummableSequence(5, 7, 11)
+    print("new_seq(100000)[-8:]:", last_8(new_seq(100000)))  # 64224133
+    #
+    # new_seq = SummableSequence(5, 7, 11)
+    # print("new_seq(100001)[-8:]:", last_8(new_seq(100001)))  # 87834603
+    #
+    # new_seq = SummableSequence(5, 7, 11)
+    # print("new_seq(100002)[-8:]:", last_8(new_seq(100002)))  # 87834603
